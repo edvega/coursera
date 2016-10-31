7229bcea6d5420f6a9ac8dd6d609843a0764b9fc
Edwin Vega <edwega@gmail.com>
2016-10-30 23:47:40 -0600

    Exercise 3 week 2, Angular Scope.

--- fullStackWebDev/angularJS/conFusion/app/scripts/app.js
+++ fullStackWebDev/angularJS/conFusion/app/scripts/app.js
@@ -1,9 +1,9 @@
 'use strict';
-angular.module('confusionApp', []).controller('menuController', function() {
-  this.tab = 1;
-  this.filtText = '';
-  
-  var dishes = [
+angular.module('confusionApp', []).controller('MenuController', ['$scope', function($scope) {
+  $scope.tab = 1;
+  $scope.filtText = '';
+  $scope.showDetails = false;
+  $scope.dishes = [
     {
       name:'Uthapizza',
       image: 'images/uthapizza.png',
@@ -43,28 +43,32 @@ angular.module('confusionApp', []).controller('menuController', function() {
       description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms',
       comment: ''
     }
-  ]; 
-  
-  this.dishes = dishes;
+  ];
+    
+    $scope.dishes = dishes;
 
-  this.select = function(setTab) {
-    this.tab = setTab;
+  $scope.select = function(setTab) {
+    $scope.tab = setTab;
     
     if (setTab === 2) {
-      this.filtText = "appetizer";
+      $scope.filtText = "appetizer";
     } 
     else if (setTab === 3) {
-      this.filtText = "mains";
+      $scope.filtText = "mains";
     }
     else if (setTab === 4) {
-      this.filtText = "dessert";
+      $scope.filtText = "dessert";
     }
     else {
-      this.filtText = "";
+      $scope.filtText = "";
     }
   };
   
-  this.isSelected = function (checkTab) {
-    return (this.tab === checkTab);
-  };
-});
+    $scope.isSelected = function (checkTab) {
+        return ($scope.tab === checkTab);
+    };
+    
+    $scope.toggleDetails = function() {
+        $scope.showDetails = !$scope.showDetails;
+    };
+}]);
