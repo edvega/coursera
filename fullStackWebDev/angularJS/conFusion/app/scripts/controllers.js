822e7ddf0b994d1a227346f608bb7f4ab9239baf
Edwin Vega <edwega@gmail.com>
2016-11-21 21:27:42 -0600

    AngularJS week 4 exercise 2, Client-Server Communication using

--- fullStackWebDev/angularJS/conFusion/app/scripts/controllers.js
+++ fullStackWebDev/angularJS/conFusion/app/scripts/controllers.js
@@ -8,7 +8,14 @@ angular.module('confusionApp')
             $scope.filtText = '';
             $scope.showDetails = false;
 
-            $scope.dishes= menuFactory.getDishes();
+            $scope.dishes = {};
+            
+            menuFactory.getDishes()
+            .then(
+                function(response) {
+                    $scope.dishes = response.data;
+                }
+            );
 
                         
             $scope.select = function(setTab) {
@@ -69,11 +76,15 @@ angular.module('confusionApp')
         }])
 
         .controller('DishDetailController', ['$scope', '$stateParams', 'menuFactory', function($scope, $stateParams, menuFactory) {
-
-            var dish= menuFactory.getDish(parseInt($stateParams.id,10));
             
-            $scope.dish = dish;
+            $scope.dish = {};
             
+            menuFactory.getDish(parseInt($stateParams.id,10))
+            .then(
+                function(response) {
+                    $scope.dish = response.data;
+                }
+            );
         }])
 
         .controller('DishCommentController', ['$scope', function($scope) {
@@ -96,14 +107,19 @@ angular.module('confusionApp')
         .controller('IndexController', ['$scope', 'menuFactory', 'corporateFactory', 
                                         function($scope, menuFactory, corporateFactory) {
             
-            var dish = menuFactory.getDish(0);
-            $scope.dish = dish;
+            var chef = corporateFactory.getLeader(3);
+            $scope.chef = chef;
+            
+            $scope.dish = {};
+            menuFactory.getDish(0)
+            .then(
+                function(response) {
+                    $scope.dish = response.data;
+                }
+            );
                                             
             var promotion = menuFactory.getPromotion(0);
             $scope.promotion = promotion;
-                                            
-            var chef = corporateFactory.getLeader(3);
-            $scope.chef = chef;
         }])
         
         .controller('AboutController', ['$scope', 'corporateFactory', function($scope, corporateFactory) {
